08e62cee4200862d276a5aaab935d0e10054ccab
unknown <fifauzi24@gmail.com>
2023-06-03 22:44:55 +0700

    No2 slide2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-// Problem Solving Basic - Test 2
 import React from 'react';
 
 class RatioCalculator extends React.Component {
-  calculateRatios = (arr) => {
-    const n = arr.length;
+  constructor(props) {
+    super(props);
+    this.state = {
+      n: 0,
+      arr: [],
+      ratios: {
+        positiveRatio: 0,
+        negativeRatio: 0,
+        zeroRatio: 0
+      }
+    };
+  }
+
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  calculateRatios = () => {
+    const { n, arr } = this.state;
+
+    const arrIntegers = arr.split(' ').map(Number); // Split the string input and convert to an array of integers
+
     let positiveCount = 0;
     let negativeCount = 0;
     let zeroCount = 0;
 
     // Count the number of positive, negative, and zero elements
-    arr.forEach((num) => {
+    arrIntegers.forEach((num) => {
       if (num > 0) {
         positiveCount += 1;
       } else if (num < 0) {
@@ -24,19 +44,49 @@ class RatioCalculator extends React.Component {
     const negativeRatio = negativeCount / n;
     const zeroRatio = zeroCount / n;
 
-    return {
+    const ratios = {
       positiveRatio,
       negativeRatio,
       zeroRatio
     };
+
+    this.setState({ ratios });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.calculateRatios();
   };
 
   render() {
-    const arr = [2, 2, -1, -2, 0];
-    const ratios = this.calculateRatios(arr);
+    const { n, arr, ratios } = this.state;
 
     return (
       <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Size of the array (n):
+            <input
+              type="number"
+              name="n"
+              value={n}
+              onChange={this.handleInputChange}
+            />
+          </label>
+          <br />
+          <label>
+            Array elements:
+            <input
+              type="text"
+              name="arr"
+              value={arr}
+              onChange={this.handleInputChange}
+            />
+          </label>
+          <br />
+          <button type="submit">Calculate Ratios</button>
+        </form>
+        <br />
         <p>{ratios.positiveRatio.toFixed(6)}</p>
         <p>{ratios.negativeRatio.toFixed(6)}</p>
         <p>{ratios.zeroRatio.toFixed(6)}</p>
